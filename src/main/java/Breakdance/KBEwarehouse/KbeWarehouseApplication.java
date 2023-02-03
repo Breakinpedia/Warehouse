2b8afa1960c748eb50d590077cee121c892a7ad9
Thien Duong <duongthien1809@gmail.com>
2023-02-03 22:29:07 +0100

    first commit

---
+++ src/main/java/Breakdance/KBEwarehouse/KbeWarehouseApplication.java
@@ -0,0 +1,13 @@
+package Breakdance.KBEwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbeWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KbeWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Breakdance/KBEwarehouse/KbeWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package Breakdance.KBEwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbeWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
